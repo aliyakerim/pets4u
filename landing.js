02f810dbf953666381eebe20f50abca8c4a7e27a
Aliya <akd.present@gmail.com>
2022-03-20 19:27:59 -0700

    swtiched the postion for the script in HTML. updated info by using DOM in JS.

---
+++ landing.js
@@ -0,0 +1,5 @@
+
+//I used DOM to access and update information in the landing page.
+
+const para = document.querySelector('.welcome-passage');
+para.innerHTML = ' Pets for you is the place where you can find information about adopting pets and licesing pets. Please take a look at our website and find your friend today.  ';
